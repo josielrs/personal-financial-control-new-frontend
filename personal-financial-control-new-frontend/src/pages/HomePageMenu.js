c9e30f3fcf5ee0a5c5fddb7eea67d656aa984121
josielrs <158061156+josielrs@users.noreply.github.com>
2025-06-21 23:23:12 -0300

    ok
    ok

--- personal-financial-control-new-frontend/src/pages/HomePageMenu.js
+++ personal-financial-control-new-frontend/src/pages/HomePageMenu.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import { ProgressBar } from 'smart-webcomponents-react/progressbar';
 import FinancialEntry from '../components/FinancialEntry';
 
 export default function HomePageMenu(){
+
+    const initialData = {
+        "description": "Sem Informações",
+        "difference": 0,
+        "expensesAmout": 0,
+        "month": 0,
+        "reservesAmount": 0,
+        "reservesPercent": 0,
+        "revenueAmout": 0,
+        "year": 0
+    }
+    const [currentControlMonthData, setCurrentControlMonthData] = useState(initialData)    
 
     const lastValidControl = "Janeiro/2025"
     const totalRevenue = "R$ 1250,00"
@@ -12,19 +25,19 @@ export default function HomePageMenu(){
         <div>
             <div>
                 <div style={{width: 301, height: 21, left: 395, top: 42, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#FD1717', fontSize: 36,
-                fontFamily: 'Poppins', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>{lastValidControl}</div>
+                fontFamily: 'Poppins', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>{currentControlMonthData.description}</div>
                 <div style={{width: 103, height: 21, left: 376, top: 81, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 12, fontFamily:
                 'Poppins', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Total Receitas</div>
                 <div style={{width: 147, height: 42, left: 377, top: 98, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#000CF2', fontSize: 24, fontFamily: 'Poppins', fontWeight:
-                '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>{totalRevenue}</div>
+                '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>{currentControlMonthData.revenueAmout}</div>
                 <div style={{width: 103, height: 21, left: 650, top: 80, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 12, fontFamily:
                 'Poppins', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Total Despesas</div>
                 <div style={{width: 195, height: 42, left: 550, top: 98, position: 'absolute', textAlign:'right', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#000CF2', fontSize: 24, fontFamily: 'Poppins', fontWeight:
-                '700', textTransform: 'capitalize', wordWrap: 'break-word', direction:'rtl'}}>{totalExpenses}</div>
+                '700', textTransform: 'capitalize', wordWrap: 'break-word', direction:'rtl'}}>{currentControlMonthData.expensesAmout}</div>
                 <div style={{width: 148, height: 21, left: 374, top: 143, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 12, fontFamily:
                 'Poppins', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Total de Reservas</div>
                 <div style={{width:'375px', left: 374, top: 168, borderRadius:'15px 15px 15px 15px',position: 'relative'}}>
-                    <ProgressBar rightToLeft={false} showProgressValue value={percent} max={100} min={0} style={{width:'375px', marginBottom:'5px', marginTop:'5px', borderRadius:'15px 15px 15px 15px', justifyContent:'center', display:'flex', color:'white', backgroundColor:'#A5F8B0'}}></ProgressBar>
+                    <ProgressBar rightToLeft={false} showProgressValue value={currentControlMonthData.reservesPercent} max={100} min={0} style={{width:'375px', marginBottom:'5px', marginTop:'5px', borderRadius:'15px 15px 15px 15px', justifyContent:'center', display:'flex', color:'white', backgroundColor:'#A5F8B0'}}></ProgressBar>
                 </div>
             </div>
 
