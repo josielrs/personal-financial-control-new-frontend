3a60c81faeb199d6fb364b6e7cd551909029aa15
josielrs <158061156+josielrs@users.noreply.github.com>
2025-06-23 19:29:02 -0300

    ok
    ok

--- personal-financial-control-new-frontend/src/pages/HomePageMenu.js
+++ personal-financial-control-new-frontend/src/pages/HomePageMenu.js
@@ -1,43 +1,71 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ProgressBar } from 'smart-webcomponents-react/progressbar';
+import { SERVER_ENDPOINT } from '../Utils'
+
 import FinancialEntry from '../components/FinancialEntry';
+import axios from 'axios'
+
+import 'smart-webcomponents-react/source/styles/smart.default.css';
 
 export default function HomePageMenu(){
 
     const initialData = {
         "description": "Sem Informações",
         "difference": 0,
-        "expensesAmout": 0,
+        "expensesAmout": "R$ 0,00",
         "month": 0,
         "reservesAmount": 0,
         "reservesPercent": 0,
-        "revenueAmout": 0,
+        "revenueAmout": "R$ 0,00",
         "year": 0
     }
     const [currentControlMonthData, setCurrentControlMonthData] = useState(initialData)    
 
-    const lastValidControl = "Janeiro/2025"
-    const totalRevenue = "R$ 1250,00"
-    const totalExpenses = "R$ 2300,00"
-    const percent = 80
+    useEffect(() => {
+      axios.get(SERVER_ENDPOINT + '/financialControl/currentSummary/')
+        .then(res => {
+            if (res.status == 200) {
+                let receivedData = res.data
+                if (receivedData.expensesAmout !== undefined && !!receivedData.expensesAmout && receivedData.expensesAmout > 0) {
+                    receivedData.expensesAmout = res.data.expensesAmout.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+                } else {
+                    receivedData.expensesAmout = "R$ 0,00"
+                }
+                if (receivedData.revenueAmout !== undefined && !!receivedData.revenueAmout && receivedData.revenueAmout > 0) {
+                    receivedData.revenueAmout = res.data.revenueAmout.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+                } else {
+                    receivedData.revenueAmout = "R$ 0,00"
+                }
+                setCurrentControlMonthData(receivedData)
+            } else if (res.status == 204) {
+                setCurrentControlMonthData(initialData)
+            } else {
+                throw new Error(res.statusText)
+            }
+          })  
+        .catch(error => {
+            console.log('failed to return summary data. ' + error)
+            setCurrentControlMonthData(initialData)
+        })
+    }, [])    
 
     return (
         <div>
             <div>
-                <div style={{width: 301, height: 21, left: 395, top: 42, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#FD1717', fontSize: 36,
+                <div style={{width: 401, height: 21, left: 365, top: 42, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#FD1717', fontSize: 36,
                 fontFamily: 'Poppins', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>{currentControlMonthData.description}</div>
                 <div style={{width: 103, height: 21, left: 376, top: 81, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 12, fontFamily:
                 'Poppins', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Total Receitas</div>
-                <div style={{width: 147, height: 42, left: 377, top: 98, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#000CF2', fontSize: 24, fontFamily: 'Poppins', fontWeight:
+                <div style={{width: 200, height: 42, left: 377, top: 98, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#000CF2', fontSize: 24, fontFamily: 'Poppins', fontWeight:
                 '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>{currentControlMonthData.revenueAmout}</div>
                 <div style={{width: 103, height: 21, left: 650, top: 80, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 12, fontFamily:
                 'Poppins', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Total Despesas</div>
                 <div style={{width: 195, height: 42, left: 550, top: 98, position: 'absolute', textAlign:'right', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#000CF2', fontSize: 24, fontFamily: 'Poppins', fontWeight:
                 '700', textTransform: 'capitalize', wordWrap: 'break-word', direction:'rtl'}}>{currentControlMonthData.expensesAmout}</div>
-                <div style={{width: 148, height: 21, left: 374, top: 143, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 12, fontFamily:
+                <div style={{width: 200, height: 21, left: 374, top: 143, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 12, fontFamily:
                 'Poppins', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Total de Reservas</div>
                 <div style={{width:'375px', left: 374, top: 168, borderRadius:'15px 15px 15px 15px',position: 'relative'}}>
-                    <ProgressBar rightToLeft={false} showProgressValue value={currentControlMonthData.reservesPercent} max={100} min={0} style={{width:'375px', marginBottom:'5px', marginTop:'5px', borderRadius:'15px 15px 15px 15px', justifyContent:'center', display:'flex', color:'white', backgroundColor:'#A5F8B0'}}></ProgressBar>
+                    <ProgressBar theme='purple' rightToLeft={false} showProgressValue value={currentControlMonthData.reservesPercent} max={100} min={0} style={{width:'375px', marginBottom:'5px', marginTop:'5px', borderRadius:'15px 15px 15px 15px', justifyContent:'center', display:'flex', color:'white', backgroundColor:'#A5F8B0'}} />
                 </div>
             </div>
 
