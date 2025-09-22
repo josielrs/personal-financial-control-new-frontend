8daa4ce41bc5ddd0e81d226bd4765ec67b2506f6
josielrs <158061156+josielrs@users.noreply.github.com>
2025-09-21 23:08:08 -0300

    ok

---
+++ personal-financial-control-new-frontend/src/components/DollarCotation.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react"
+import { showInfoMessage } from '../Utils'
+
+import axios from 'axios'
+
+
+export default function DollarCotation(props){
+
+    const [cotationValue, setCotationValue] = useState([])
+
+    const dataAtual = new Date()
+    const ano = dataAtual.getFullYear();
+    const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
+    const dia = String(dataAtual.getDate()).padStart(2, '0');   
+
+    let serviceUrl = 'https://brasilapi.com.br/api/cambio/v1/cotacao/USD/' + `${ano}${mes}${dia}`
+
+    useEffect(() => {
+      axios.get(serviceUrl)
+        .then(res => {
+            if (res.status === 200) {
+                if (!!res.data.cotacoes) {
+                    for (let item of res.data.cotacoes){
+                        if (!!item && !!item.cotacao_venda) {
+                            setCotationValue(item.cotacao_venda);
+                            break;
+                        }
+                    }
+                }
+            } else {
+                throw new Error(res.statusText)
+            }
+          })  
+        .catch(error => showInfoMessage("Falha ao Retornar dados do servidor: " + error.message))
+    }, [])
+
+        return (
+            <label>Cotação do Dolar Hoje {cotationValue.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</label>
+        )
+}    
--- personal-financial-control-new-frontend/src/components/MainContent.js
+++ personal-financial-control-new-frontend/src/components/MainContent.js
@@ -1,5 +1,6 @@
 import AppLogo from "../assets/AppLogoInBase64";
 import Menu from "./Menu";
+import DollarCotation from "./DollarCotation";
 
 export default function MainContent(){
 
@@ -7,6 +8,7 @@ export default function MainContent(){
         <div>
             <div data-layer="smartFinTitle" className="Smartfintitle" style={{left: 316, top: 108, position: 'absolute', color: 'black', fontSize: 20.11, fontFamily: 'Poppins', fontWeight: '250', wordWrap: 'break-word'}}>Sua ferramenta de gestão financeira pessoal</div>
             <div data-layer="smartFinName" className="Smartfinname" style={{left: 316, top: 61, position: 'absolute', color: 'black', fontSize: 43.37, fontFamily: 'Lexend Mega', fontWeight: '700', wordWrap: 'break-word'}}>SMART FIN</div>
+            <div data-layer="smartFinName" className="Smartfinname" style={{left: 1016, top: 100, position: 'absolute', color: 'black', fontSize: 13.37, fontFamily: 'Lexend Mega', fontWeight: '700', wordWrap: 'break-word'}}><DollarCotation/></div>
             <Menu/>
             <img data-layer="applogo" className="Applogo" style={{width: 126, height: 155, left: 73, top: 48, position: 'absolute'}} alt="" src={AppLogo}/>               
         </div> 
